021ea13c57933c353252724d0b686dccc03c69a0
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-13 20:14:01 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,13 +2,27 @@ class Owner
   # code goes here
   attr_reader :name, :species
   
+  @@all = []
+  
   def initialize(name)
     @name = name
     @species = 'human'
+    @@all << self
   end
   
   def say_species
     "I am a #{self.species}."
   end
   
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all
+    @@all = []
+  end
 end
