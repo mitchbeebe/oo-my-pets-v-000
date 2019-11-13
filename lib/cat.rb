4619da40c307630537e285c2ea8ddfd606bbda2c
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-13 20:17:38 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,4 +1,11 @@
 class Cat
   # code goes here
+  attr_reader :name, :owner
+  attr_accessor :mood
   
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+  end
 end
