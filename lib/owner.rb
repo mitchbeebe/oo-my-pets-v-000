c7ec651c74a4edef1ba5d20ac3ce8e898632e21c
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-13 20:30:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -67,7 +67,7 @@ class Owner
   end
   
   def list_pets
-    "I have #{self.dogs.length} dog(s), and #{self.cats.length}."
+    "I have #{self.dogs.length} dog(s), and #{self.cats.length} cat(s)."
   end
     
 end
